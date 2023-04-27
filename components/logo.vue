fa4511cec65d18bc5f85cbdb6626c34ed9076bb6
Uliana Kolodii <ulianakolodii@gmail.com>
2023-04-27 11:32:45 +0300

    feat: add logo animation

--- components/logo.vue
+++ components/logo.vue
@@ -1,7 +1,26 @@
+<script setup>
+const firstLineRef = ref(null);
+const secondLineRef = ref(null);
+
+const startLogo = () => {
+  firstLineRef.value.classList.remove("running-right");
+  firstLineRef.value.classList.add("running-left");
+  secondLineRef.value.classList.remove("running-left");
+  secondLineRef.value.classList.add("running-right");
+};
+
+const endLogo = () => {
+  firstLineRef.value.classList.remove("running-left");
+  firstLineRef.value.classList.add("running-right");
+  secondLineRef.value.classList.remove("running-right");
+  secondLineRef.value.classList.add("running-left");
+};
+</script>
+
 <template>
-  <div class="logo">
-    <p>PORT</p>
-    <p class="second_line">FOLIO</p>
+  <div class="logo" @mouseover="startLogo" @mouseleave="endLogo">
+    <p class="first_line" ref="firstLineRef">PORT</p>
+    <p class="second_line" ref="secondLineRef">FOLIO</p>
   </div>
 </template>
 
@@ -9,12 +28,55 @@
 .logo {
   display: flex;
   flex-direction: column;
-  padding-left: 40px;
+  margin-left: 40px;
   font-weight: 400;
   font-size: 30px;
+  overflow: hidden;
+}
+
+.first_line {
+  width: 80px;
 }
 
 .second_line {
+  width: 90px;
   margin-left: 30px;
+}
+
+.running-left {
+  animation: running-left 0.5s linear forwards;
+}
+.running-right {
+  animation: running-right 0.5s linear forwards;
+}
+
+@keyframes running-left {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(150%);
+  }
+  51% {
+    transform: translateX(-150%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes running-right {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(-150%);
+  }
+  51% {
+    transform: translateX(150%);
+  }
+  100% {
+    transform: translateX(0);
+  }
 }
 </style>
